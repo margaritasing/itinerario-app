4c0c1640500ab0c9bac5430050739b4a07051032
margaritasing <libiamfreitesm@gmail.com>
2022-04-04 18:16:57 -0300

    subo cambios

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors())
 app.use(passport.initialize())
 app.use("/api",Router)
 
-if (procces.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
     app.get('*', (req, res)=>{
         res.sendFile(path.join(__dirname+'/client/build/index.html'))
